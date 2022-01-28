dd308deb1ba75c9baade011deaa9345a9a188441
marclloret <mlloretd@edu.tecnocampus.cat>
2022-01-29 00:02:09 +0100

    a

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -58,7 +58,7 @@ public class PlayerAnimations : MonoBehaviour
         print(vector.x);
         animator.SetFloat("speed", Math.Abs(vector.x));
 
-        if ((dreta && Mathf.Sign(vector.x) < 0) || (!dreta && Mathf.Sign(vector.x) > 0))
+        if ((dreta && vector.x < 0) || (!dreta && vector.x > 0))
         {
              transform.Rotate(new Vector3(0, 180, 0));
              dreta = !dreta;
