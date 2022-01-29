0c8973069b7a307bf5e545884e93bdea1a3803b6
CoraReal <creal@edu.tecnocampus.cat>
2022-01-29 19:13:40 +0100

    Menu changes (Cuidado amb aquest commit no vui cagarla)

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -14,14 +14,15 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        if (instance == null)
-            instance = this;
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
+        //if (instance == null)
+        //    instance = this;
+        //else
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+
+        //DontDestroyOnLoad(gameObject);
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
---
+++ Assets/Scripts/Menu/CreditsExit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+            SceneManager.LoadScene("Menu");
+    }
+}
