656cebbcef3d5b825a50b124c25139a62a606921
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-01-29 19:36:57 +0100

    Transition

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -7,10 +7,22 @@ public class EndLevel : MonoBehaviour
 {
     [SerializeField]
     int nextLvl;
+    [SerializeField]
+    float transitionTime;
+
+    [SerializeField]
+    Animator transition;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        StartCoroutine(LoadLevel());
         //SceneManager.LoadSceneAsync(nextLvl);
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        //SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    IEnumerator LoadLevel(){
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadSceneAsync(nextLvl);
     }
 }
