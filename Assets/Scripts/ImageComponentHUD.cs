9eae21c7c62f95894617c95d796d07c94a911695
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-01-30 00:09:29 +0100

    Update ImageComponentHUD.cs

--- Assets/Scripts/ImageComponentHUD.cs
+++ Assets/Scripts/ImageComponentHUD.cs
@@ -10,21 +10,56 @@ public class ImageComponentHUD : MonoBehaviour
     public Sprite originalSprite;
     public Sprite pressedSprite;
 
+    ColorManager colorManager;
 
+    private static ImageComponentHUD _instance;
+    public static ImageComponentHUD Instance => _instance;
+
+    private Controls controls;
+    private Controls Controls{
+        get{
+            if(controls != null) {return controls;}
+            return controls = new Controls();
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = gameObject.GetComponent<ImageComponentHUD>();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+     private void OnEnable()
+    {
+        GameObject CM = GameObject.FindWithTag("Color Manager");
+        colorManager = CM.GetComponent<ColorManager>();
+        Controls.Scenario.SwitchColors.Enable();
+        Controls.Scenario.SwitchColors.performed += _ =>
+        {
+           if(colorManager._SwitchColor){//if true is on
+                myImageComponent.sprite = pressedSprite;
+           }else{myImageComponent.sprite = originalSprite;}
+        };
+    }
+
+    private void OnDisable()
+    {
+        Controls.Scenario.SwitchColors.Disable();
+        Controls.Scenario.SwitchColors.performed -= _ =>
+        {
+            
+        };
+    }
     void Start() 
     {
         myImageComponent = GetComponent<Image>();
     }
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            myImageComponent.sprite = pressedSprite;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            myImageComponent.sprite = originalSprite;
-        }
-    }
+    
 }
