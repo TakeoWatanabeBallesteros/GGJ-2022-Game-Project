1211765f64606ee79ce78f7a42a0c67dbdfeccef
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-02-02 17:28:16 +0100

    UI menu with gamepad

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    Button MainMenuInitButton;
+    [SerializeField]
+    Slider OptionsInitButton;
     public void Play()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -21,17 +26,16 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         #if UNITY_STANDALONE
-        Application.Quit();
-#endif
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
         #endif
     }
-
-    void Update()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-
-       
+    public void SelectOptionsButton(){
+        OptionsInitButton.Select();
+    }
+    public void SelectMainMenuButton(){ 
+        MainMenuInitButton.Select();
     }
 }
