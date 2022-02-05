8b56c87e0815bc735977041edb8482bcf193712f
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-02-05 19:19:12 +0100

    Update JoystickPosition.cs

--- Assets/Scripts/JoystickPosition.cs
+++ Assets/Scripts/JoystickPosition.cs
@@ -21,15 +21,21 @@ public class JoystickPosition : MonoBehaviour
     private void OnEnable() {
         Controls.Touch.TouchPress.Enable();
         Controls.Touch.TouchPos.Enable();
-        Controls.Touch.TouchPress.performed += _ => {Joystick.enabled = true;};
+        Controls.Touch.TouchPress.performed += _ => SetJoystick();
         Controls.Touch.TouchPress.canceled += _ => {Joystick.enabled = false;};
-        Controls.Touch.TouchPos.performed += ctx => {transform.position = ctx.ReadValue<Vector2>();};
     }
     private void OnDisable() {
         Controls.Touch.TouchPress.Disable();
         Controls.Touch.TouchPos.Disable();
-        Controls.Touch.TouchPress.performed -= _ => {Joystick.enabled = true;};
+        Controls.Touch.TouchPress.performed -= _ => SetJoystick();
         Controls.Touch.TouchPress.canceled -= _ => {Joystick.enabled = false;};
-        Controls.Touch.TouchPos.performed -= ctx => {transform.position = ctx.ReadValue<Vector2>();};
+    }
+
+    void SetJoystick(){
+        Joystick.enabled = true;
+        Controls.Touch.TouchPos.performed += ctx => {
+            Vector2 touchPos = ctx.ReadValue<Vector2>();
+            if(touchPos.x < Screen.width/2 && touchPos.y > Screen.height/2)
+                transform.position = touchPos;};
     }
 }
