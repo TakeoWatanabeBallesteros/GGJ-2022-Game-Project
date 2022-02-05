cb10114c78397932de078f1cd7a1965592f30485
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-02-05 06:35:05 +0100

    smooth joystick

--- Assets/Scripts/JoystickPosition.cs
+++ Assets/Scripts/JoystickPosition.cs
@@ -16,28 +16,20 @@ public class JoystickPosition : MonoBehaviour
     }
     private void Awake() {
         Joystick = gameObject.GetComponent<Image>();
-        //Joystick.enabled = false;
+        Joystick.enabled = false;
     }
     private void OnEnable() {
-        Controls.Scenario.MousePos.Enable();
-        Controls.Scenario.MousePos.performed += ctx => {transform.position = new Vector3(ctx.ReadValue<Vector2>().x,ctx.ReadValue<Vector2>().y, 0);};
-        //Controls.Touch.TouchPress.Enable();
+        Controls.Touch.TouchPress.Enable();
         Controls.Touch.TouchPos.Enable();
         Controls.Touch.TouchPress.performed += _ => {Joystick.enabled = true;};
         Controls.Touch.TouchPress.canceled += _ => {Joystick.enabled = false;};
-        Controls.Touch.TouchPos.performed += ctx => {transform.position = new Vector3(ctx.ReadValue<Vector2>().x,ctx.ReadValue<Vector2>().y, 0);};
+        Controls.Touch.TouchPos.performed += ctx => {transform.position = ctx.ReadValue<Vector2>();};
     }
     private void OnDisable() {
         Controls.Touch.TouchPress.Disable();
         Controls.Touch.TouchPos.Disable();
         Controls.Touch.TouchPress.performed -= _ => {Joystick.enabled = true;};
         Controls.Touch.TouchPress.canceled -= _ => {Joystick.enabled = false;};
-        Controls.Touch.TouchPos.performed -= ctx => {transform.position = new Vector3(ctx.ReadValue<Vector2>().x,ctx.ReadValue<Vector2>().y, 0);};
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Controls.Touch.TouchPos.performed -= ctx => {transform.position = ctx.ReadValue<Vector2>();};
     }
 }
